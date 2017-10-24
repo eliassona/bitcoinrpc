bf786d699dc3c5b17fcc6e91cee5934c4842d7c5
Anders Eliasson <anders.eliasson@digitalroute.com>
2017-10-24 15:35:49 +0200

    try debug-repl

--- project.clj
+++ project.clj
@@ -6,6 +6,10 @@
   :java-source-paths ["java/src"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [clj-http "3.7.0"]
+                 [com.gfredericks/debug-repl "0.0.9"]
                  [org.clojure/data.json "0.2.6"]
                  [instaparse "1.4.7"]
-                 [org.clojure/test.check "0.9.0"]])
+                 [org.clojure/test.check "0.9.0"]]
+  :repl-options
+  {:nrepl-middleware
+    [com.gfredericks.debug-repl/wrap-debug-repl]})
