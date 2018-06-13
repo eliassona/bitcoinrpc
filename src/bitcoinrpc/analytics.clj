018f6da8405567e9742e8ee5acb9e3d9d49b30ee
Anders Eliasson <anders.eliasson@digitalroute.com>
2018-06-13 07:06:38 +0200

    added lazy seq for blocks

--- src/bitcoinrpc/analytics.clj
+++ src/bitcoinrpc/analytics.clj
@@ -2,16 +2,43 @@
   "Bitcoin Core analytics"
   (:use [clojure.pprint]
         [clojure.repl])
-  (:require [bitcoinrpc.core :refer [getrawtransaction config]]))
+  (:require [bitcoinrpc.core :refer :all]))
 
 
+(def coinbase-tx (apply str (repeat 32 "0")))
 
-  (defn tx-history-of [txid]
-    
-    )
+(defn tx-history-of [txid]
+  (let [tx (-> txid getrawtransaction decoderawtransaction)
+        tx-id (tx "txid")]
+    (if (= tx-id coinbase-tx)
+      tx
+      (let [vin (tx "vin")]
+        tx))))
+        ;(lazy-seq (cons tx (tx-history-of  
   
-  (defn tx-future-of [txid]
-    
-    )
+(defn blocks-of [the-key]
+  (letfn [(block-fn [block-hash]
+          (let [b (getblock block-hash)]
+            (if-let [bh (b the-key)]
+              (lazy-seq (cons b (block-fn bh)))
+              (list b))))]
+    block-fn))
+
+(defn block-hash-str-of [prefix] (str prefix "blockhash"))
+
+(defn blocks-backward
+  "Get a lazy seq of blocks going backward in time"
+  ([block-hash]
+  ((blocks-of (block-hash-str-of "prevous")) block-hash))
+  ([]
+    (blocks-backward (getbestblockhash))))
+
+(defn blocks-forward
+  "Get a lazy seq of blocks going forward in time"
+  ([block-hash]
+  ((blocks-of (block-hash-str-of "next")) block-hash))
+  ([]
+    (blocks-backward (getblockhash 0))))
   
+        
   
